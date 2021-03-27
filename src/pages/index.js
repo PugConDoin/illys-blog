ff291db454c9b632ad4861b557369d94f758c306
Mark Freeman <dorsettri@gmail.com>
2021-03-27 15:05:36 +0000

    add 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,32 @@
 import { graphql, Link as GatsbyLink } from 'gatsby'
 import React from 'react'
 import { Box, Heading, Link } from 'theme-ui'
-import { Layout } from '../components/layout.js'
 
 export default function IndexPage({ data }) {
   return (
     <>
-      <Layout>
-        {data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
-          <Box
-            key={id}
-            as='article'
-            sx={{
-              mb: 4,
-              p: 3,
-              boxShadow: '0 5px 7px -2px rgba(0, 0, 0, 0.1)',
-              borderRadius: '5px',
-            }}
-          >
-            <Link as={GatsbyLink} to={`/${slug}`}>
-              <Heading>{frontmatter.title}</Heading>
-              <Box as='p' variant='styles.p'>
-                {frontmatter.date}
-              </Box>
-              <Box as='p' variant='styles.p'>
-                {excerpt}
-              </Box>
-            </Link>
-          </Box>
-        ))}
-      </Layout>
+      {data.allMdx.nodes.map(({ id, excerpt, frontmatter, slug }) => (
+        <Box
+          key={id}
+          as='article'
+          sx={{
+            mb: 4,
+            p: 3,
+            boxShadow: '0 5px 7px -2px rgba(0, 0, 0, 0.1)',
+            borderRadius: '5px',
+          }}
+        >
+          <Link as={GatsbyLink} to={`/${slug}`}>
+            <Heading>{frontmatter.title}</Heading>
+            <Box as='p' variant='styles.p'>
+              {frontmatter.date}
+            </Box>
+            <Box as='p' variant='styles.p'>
+              {excerpt}
+            </Box>
+          </Link>
+        </Box>
+      ))}
     </>
   )
 }
